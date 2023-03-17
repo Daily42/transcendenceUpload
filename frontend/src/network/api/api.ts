a5e029a1f160636ca0da5d1c6d202aec13e15448
KKWANH <kwanho0096@gmail.com>
2023-03-17 15:47:51 +0900

    api add

---
+++ frontend/src/network/api/api.ts
@@ -0,0 +1,58 @@
+import * as axios from "./axios.instance";
+import DateType from "../../enum/dateType.enum";
+import PlaceType from "../../enum/placeType.enum";
+
+export const getEventsList = async () => {
+  try {
+    const response = await axios.instance.get("/events");
+    return (response);
+  } catch (error) {
+    return (error);
+  }
+}
+
+export const getEvent = async (eventId: string) => {
+  try {
+    const response = await axios.instance.get(`/events/${eventId}`);
+    return (response);
+  } catch (error) {
+    return (error);
+  }
+}
+
+export const getEventType = async () => {
+  try {
+    const response = await axios.instance.get("/event-type");
+    return (response);
+  } catch (error) {
+    return (error);
+  }
+}
+
+export const getLocationsList = async () => {
+  try {
+    const response = await axios.instance.get("/locations");
+    return (response);
+  } catch (error) {
+    return (error);
+  }
+}
+
+export const getLocation = async (locId: PlaceType) => {
+  try {
+    const response = await axios.instance.get(`/locations/${locId}`);
+    return (response);
+  } catch (error) {
+    return (error);
+  }
+}
+
+export const getChildLocations = async (locId: PlaceType) => {
+  try {
+    const response = await axios.instance.get(`/locations/children/${locId}`);
+    return (response);
+  } catch (error) {
+    return (error);
+  }
+}
+//event-type
--- frontend/src/network/api/axios.instance.ts
+++ frontend/src/network/api/axios.instance.ts
@@ -2,19 +2,24 @@ import axios from "axios";
 import store from "../../redux/store";
 import { AuthData, setAuth } from "../../redux/slices/auth";
 
-export const instance = axios.create({
+export const instance2 = axios.create({
   baseURL: `${window.location.origin}/api`,
   timeout: 5000,
 });
 
-instance.interceptors.response.use((response) => {
-  store.dispatch(setAuth({ auth: true } as AuthData));
-  return response;
-}, (error) => {
-  if (error.response.status === 401) {
-    store.dispatch(setAuth({ auth: false } as AuthData));
-  }
-  return Promise.reject(error);
+export const instance = axios.create({
+  baseURL: "http://daily42-env.eba-dmbiy4zs.ap-northeast-2.elasticbeanstalk.com",
+  timeout: 5000,
 });
+
+// instance.interceptors.response.use((response) => {
+//   store.dispatch(setAuth({ auth: true } as AuthData));
+//   return response;
+// }, (error) => {
+//   if (error.response.status === 401) {
+//     store.dispatch(setAuth({ auth: false } as AuthData));
+//   }
+//   return Promise.reject(error);
+// });
 
 export default instance;
